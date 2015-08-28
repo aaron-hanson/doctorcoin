f8e54600d505ca981e1b0a14bcd4619b4786a788
Aaron Hanson <archaeal@gmail.com>
2015-08-28 15:02:50 -0400

    epoch time and newspaper headline

--- src/main.cpp
+++ src/main.cpp
@@ -2784,7 +2784,7 @@ bool InitBlockIndex() {
         //   vMerkleTree: 97ddfbbae6
 
         // Genesis block
-        const char* pszTimestamp = "NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56";
+        const char* pszTimestamp = "Chicago Tribune 28/Aug/2015 Stalemate Building On Itself";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -2796,13 +2796,13 @@ bool InitBlockIndex() {
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1317972665;
+        block.nTime    = 1440787916;
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 2084524493;
 
         if (fTestNet)
         {
-            block.nTime    = 1317798646;
+            block.nTime    = 1440787916;
             block.nNonce   = 385270584;
         }
 
